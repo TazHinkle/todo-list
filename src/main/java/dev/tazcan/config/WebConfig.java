1d99480b8bf3463bd4d55a2a6e7a15c45f64e7fa
Taz Hinkle <hinkle.taz@gmail.com>
2020-08-22 15:45:41 -0600

    Setup Dispatcher Servlet and simple Controller

---
+++ src/main/java/dev/tazcan/config/WebConfig.java
@@ -0,0 +1,11 @@
+package dev.tazcan.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "dev.tazcan")
+public class WebConfig {
+}
