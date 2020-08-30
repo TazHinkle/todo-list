41bfc31c9be3e91fcc9e7e36f586d909d83597f2
Taz Hinkle <hinkle.taz@gmail.com>
2020-08-29 21:18:39 -0600

    ViewResolver to .jsp file added.

--- src/main/java/dev/tazcan/controller/DemoController.java
+++ src/main/java/dev/tazcan/controller/DemoController.java
@@ -7,10 +7,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DemoController {
 
+    int visits = 0;
     // http://localhost:8080/todo-list/hello
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
-        return "Hello";
+        visits++;
+        return "visits " + visits;
+    }
+
+    // http://localhost:8080/todo-list/welcome
+    @GetMapping("welcome")
+    public String welcome() {
+        return "welcome";
     }
 }
