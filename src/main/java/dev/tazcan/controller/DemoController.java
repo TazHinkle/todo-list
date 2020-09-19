3611aaa839027309f786c855ab5a0dc453727848
Taz Hinkle <hinkle.taz@gmail.com>
2020-09-19 12:36:49 -0600

    Added DemoService controllers and implementation with @Service annotation

--- src/main/java/dev/tazcan/controller/DemoController.java
+++ src/main/java/dev/tazcan/controller/DemoController.java
@@ -1,13 +1,27 @@
 package dev.tazcan.controller;
 
+import dev.tazcan.service.DemoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
 
     int visits = 0;
+    private final DemoService demoService;
+
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+    // Request methods
     // http://localhost:8080/todo-list/hello
     @ResponseBody
     @GetMapping("/hello")
@@ -18,7 +32,16 @@ public class DemoController {
 
     // http://localhost:8080/todo-list/welcome
     @GetMapping("welcome")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage("Taz"));
+        log.info("model = {}", model);
         return "welcome";
+    }
+
+    // Model attributes
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+        return demoService.getWelcomeMessage();
     }
 }
---
+++ src/main/java/dev/tazcan/service/DemoService.java
@@ -0,0 +1,8 @@
+package dev.tazcan.service;
+
+public interface DemoService {
+    String getHelloMessage(String user);
+
+    String getWelcomeMessage();
+
+}
